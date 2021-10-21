005cd0c86fa0ac01829b64aa3e2f1e749220078b
Kusap <92926311+Kusap@users.noreply.github.com>
2021-10-21 17:58:17 +0300

    Update book.js
    стопка - wrong meaning, semiEnglish (stack - стак) will be better

--- loc/items/book.js
+++ loc/items/book.js
@@ -12,7 +12,7 @@ export default [{
 
   description: {
     en: 'Slash your enemy, striking with extra force. Heals you for 5% of the damage done.',
-    ru: 'Режь врага, нанося удар с дополнительной силой. Исцеляет вас за 5% от нанесенного урона.',
+    ru: 'Режет врага, нанося удар с дополнительной силой. Исцеляет вас на 5% от нанесенного урона.',
     fr: 'Vous lacérez votre ennemi en frappant avec une force supplémentaire. Vous soigne 5% des dégats infligés.'
   }
 },
@@ -21,7 +21,7 @@ export default [{
 
   description: {
     en: 'Increase your block chance, while raising your damage for each successful block.', // descriptions are coming from the buffs
-    ru: 'Увеличьте шанс блока, одновременно повышая урон для каждого успешного блока.',
+    ru: 'Увеличьте шанс блока, одновременно повышая урон после каждого успешного блока.',
     fr: 'Vous augmentez vos chances de bloquer une attaque, tout en augmentant vos dégats pour chaque blocage réussi.'
   }
 },
@@ -30,7 +30,7 @@ export default [{
 
   description: {
     en: 'Swiftly swing your sword around you, damaging enemies within a radius.',
-    ru: 'Быстро размахивайте мечом вокруг себя, нанося урон врагам в радиусе.',
+    ru: 'Быстро взмахните мечом вокруг себя, нанеся урон врагам вокруг.',
     fr: 'Vous faites rapidement tourner votre épée autour de vous, infligeant des dégats aux ennemies dans un rayon.'
   }
 },
@@ -39,7 +39,7 @@ export default [{
 
   description: {
     en: 'Fires a missile of frost towards your enemy. Reduces the cooldown of Icicle Orb by 0.5 seconds. Freezes targets for up to 5 stacks, at which they will be stunned and take 50% increased damage. Gains one instant cast every 7 seconds.',
-    ru: 'Стреляет ракетой мороза в сторону врага. Уменьшает время перезагрузки Icicle Orb на 0,5 секунды. Замораживает цели до 5 стопок, при которых они будут оглушены и получат 50% увеличенного урона.Получает одно мгновенное заклинание каждые 7 секунд.',
+    ru: 'Стреляет ракетой льда в сторону врага. Уменьшает время перезарядки Icicle Orb на 0,5 секунды. Замораживает цель до 5 стаков, при которых она будут оглушена и будет получать на 50% увеличенный урон. Получает одно мгновенное заклинание каждые 7 секунд.',
     fr: 'Tire un projectile de glace vers votre ennemi. Réduit le temps de recharge d\'Icicle Orb de 0.5 secondes. Gèle les cibles jusqu\'à 5 charges, au cours desquelles elles seront étourdies et subiront 50% de dégats supplémentaires. Gagne un lancement instantané toutes les 7 secondes.'
   }
 },
@@ -57,7 +57,7 @@ export default [{
 
   description: {
     en: 'Heal a friendly target. Amount is increased for each stack of Revitalize.',
-    ru: 'Исцелить дружелюбную цель. Количество увеличивается за каждую стопку Revtialize.',
+    ru: 'Исцелить дружелюбную цель. Количество увеличивается за каждый стак Revtialize.',
     fr: 'Vous soignez une cible alliée. La quantité de soin est augmentée pour chaque charge de Revitalize.'
   }
 },
@@ -66,7 +66,7 @@ export default [{
 
   description: {
     en: 'Heal a friendly target over a short duration, stacking up to 3 times while also increasing the power of your Mend.',
-    ru: 'Исцелить дружелюбную цель в течение короткого времени, укладываясь до 3 раз, при этом увеличивая силу Mend.',
+    ru: 'Исцелить дружелюбную цель в течение короткого времени, складывается до 3 раз, при этом увеличивает силу Mend.',
     fr: 'Vous soignez une cible alliée sur une courte durée, cumulable jusqu\'à 3 fois, tout en augmentant la puissance du Mend.'
   }
 },
@@ -75,7 +75,7 @@ export default [{
 
   description: {
     en: 'You inherited special abilities through your ancestry and family traditions, granting you extra benefits from certain stats.',
-    ru: 'Вы унаследовали особые способности благодаря своим предкам и семейным традициям, предоставляя вам дополнительные преимущества от определенных характеристик.',
+    ru: 'Вы унаследовали особые способности благодаря своим предкам и семейным традициям, что предоставляет вам дополнительные преимущества от определенных характеристик.',
     fr: 'Vous avez hérité de capacités spéciales grâce à vos ancêtres et à vos traditions familiales, vous accordant des avantages supplémentaires sur certaines statistiques.'
   }
 },
@@ -84,7 +84,7 @@ export default [{
 
   description: {
     en: 'A carefully aimed, high damage shot. Increases the damage of your next Swift Shots and allows them to be cast instantly.',
-    ru: 'Тщательно прицельный выстрел с высоким уроном. Увеличивает урон от следующих выстрелов Swift и позволяет их мгновенно выстрелить.',
+    ru: 'Тщательно прицельный выстрел с высоким уроном. Увеличивает урон от следующих выстрелов Swift Shot и позволяет стрелять ими мгновенно.',
     fr: 'Un tir soigneusement visé, aux dégats élevés. Augmente les dégats de vos prochains Swift Shots et leur permet d\'être tirés instantanément.'
   }
 },
@@ -93,7 +93,7 @@ export default [{
 
   description: {
     en: 'Your Precise Shots will jump to additional targets while active.',
-    ru: 'Ваши Precise Shhots будут перепрыгивать на дополнительные цели, пока это заклинание активно.',
+    ru: 'Ваш Precise Shots будут перепрыгивать на дополнительные цели.',
     fr: 'Vos Precise Shots passeront à des cibles supplémentaires tant que ce sort est actif.'
   }
 },
@@ -120,7 +120,7 @@ export default [{
 
   description: {
     en: 'You and your party members quickly regenerate mana over a short period of time.',
-    ru: 'Вы и члены вашей партии быстро восстанавливаете ману за короткий период времени.',
+    ru: 'Вы и члены вашей пати быстро восстанавливаете ману в течение некоторого времени.',
     fr: 'Vous et les membres de votre groupe régénérez rapidement du mana pendant une courte durée.'
   }
 },
@@ -129,7 +129,7 @@ export default [{
 
   description: {
     en: 'Emit a chilling shockwave of ice around you, damaging and freezing enemies. Increases the critical hit chance of some of your spells.',
-    ru: 'Излучайте ледяную ударную волну вокруг вас, повреждая и замерзая врагов. Увеличивает шанс критического удара по некоторым из ваших заклинаний.',
+    ru: 'Выпустите ледяную ударную волну вокруг вас, нанося периодический урон и замораживая врагов. Увеличивает шанс критического удара для некоторымх из ваших заклинаний.',
     fr: 'Vous émettez une onde de choc glaciale autour de vous, infligeant des dégats et gelant les ennemis. Augmente les chances de coup critique de certains de vos sorts.'
   }
 },
@@ -138,7 +138,7 @@ export default [{
 
   description: {
     en: 'Summon a large orb, ejecting icicles hitting all enemies in its path.',
-    ru: 'Вызывайте большой шар, выбрасывая сосульки, ударяя всех врагов на своем пути.',
+    ru: 'Вызывите большой шар, выпускиющий сосульки и наносящий урон всем врагам на своем пути.',
     fr: 'Vous invoquez un grand orbe, éjectant des stalactites qui frappent tous les ennemis sur son chemin.'
   }
 },
@@ -147,7 +147,7 @@ export default [{
 
   description: {
     en: 'You gain Haste and all your damage output is increased. Resets the cooldown of Icicle Orb.',
-    ru: 'Вы получаете Haste и все ваши повреждения продукции увеличивается. Сбрасывает перезагрузку Icicle Orb.',
+    ru: 'Вы получаете Haste и весь ваш исходящий урон увеличивается. Сбрасывает перезарядку Icicle Orb.',
     fr: 'Vous gagnez de la Hâte et tous vos dégats sont augmentés. Réinitialise le temps de recharge d\'Icicle Orb.'
   }
 },
@@ -165,7 +165,7 @@ export default [{
 
   description: {
     en: 'Your Crescent Swipe lacerates enemies, causing them to bleed for additional Damage. Stacks up to 3 times.',
-    ru: 'Ваш Crescent Swipe рвет врагов, заставляя их истекать кровью для дополнительного урона. Складывается до 3-х раз.',
+    ru: 'Ваш Crescent Swipe рвет врагов, заставляя их истекать кровью и нанося дополнительный урон. Складывается до 3-х раз.',
     fr: 'Votre Crescent Swipe lacère les ennemis, les faisant saigner pour des dégats supplémentaires. Cumulable jusqu\'à 3 fois.'
   }
 },
@@ -174,7 +174,7 @@ export default [{
 
   description: {
     en: 'You and your party members deal additional Damage.',
-    ru: 'Вы и члены вашей party наносите дополнительный ущерб.',
+    ru: 'Вы и члены вашей пати наносите дополнительный урон.',
     fr: 'Vous et les membres de votre groupe infligez des dégats supplémentaires.'
   }
 },
@@ -183,7 +183,7 @@ export default [{
 
   description: {
     en: 'You and your party members gain additional Defense.',
-    ru: 'Вы и члены вашей party получаете дополнительную защиту.',
+    ru: 'Вы и члены вашей пати получаете дополнительную защиту.',
     fr: 'Vous et les membres de votre groupe gagnez de la Défense supplémentaire.'
   }
 },
@@ -192,7 +192,7 @@ export default [{
 
   description: {
     en: 'Passively increase your Defense.',
-    ru: 'Пассивно увеличивайте свою защиту.',
+    ru: 'Пассивно увеличивет вашу защиту.',
     fr: 'Augmente passivement votre Défense.'
   }
 },
@@ -201,7 +201,7 @@ export default [{
 
   description: {
     en: 'You and your party members gain additional Crit %.',
-    ru: 'Вы и члены вашей party получаете дополнительный Крит %',
+    ru: 'Вы и члены вашей пати получаете дополнительный Крит %',
     fr: 'Vous et les membres de votre groupe gagnez du Critique % supplémentaire.'
   }
 },
@@ -219,7 +219,7 @@ export default [{
 
   description: {
     en: 'Increase your target\'s Damage.',
-    ru: 'Увеличивайте урон вашей цели.',
+    ru: 'Увеличивет урон вашей цели.',
     fr: 'Vous augmentez les dégats de votre cible.'
   }
 },
@@ -228,7 +228,7 @@ export default [{
 
   description: {
     en: 'You and your party members gain additional Haste.',
-    ru: 'Вы и члены вашей party получаете дополнительный Haste.',
+    ru: 'Вы и члены вашей пати получаете дополнительный Haste.',
     fr: 'Vous et les membre de votre groupe gagnez de la Hâte supplémentaire.'
   }
 },
@@ -237,7 +237,7 @@ export default [{
 
   description: {
     en: 'Passively increase your Crit %.',
-    ru: 'Пассивно увеличьте свой Крит %.',
+    ru: 'Пассивно увеличивает ваш Крит %.',
     fr: 'Augmente passivement votre Critique %.'
   }
 },
@@ -246,7 +246,7 @@ export default [{
 
   description: {
     en: 'You and your party members gain additional Movement Speed.',
-    ru: 'Вы и члены вашей party получаете дополнительный Movement Speed',
+    ru: 'Вы и члены вашей пати получаете дополнительную скорость передвижения',
     fr: 'Vous et les membres de votre groupe gagnez de la Vitesse de déplacement supplémentaire.'
   }
 },
@@ -255,7 +255,7 @@ export default [{
 
   description: {
     en: 'You and your party members become enraged with Haste, enabling you to attack faster.',
-    ru: 'Вы и члены вашей party получаете значительно больше Haste, что позволяет вам атаковать быстрее.',
+    ru: 'Вы и члены вашей пати получаете значительно больше Haste, что позволяет вам атаковать быстрее.',
     fr: 'Vous et les membres de votre groupe devenez enragés par la Hâte, vous permettant d\'attaquer plus rapidement.'
   }
 },
@@ -264,7 +264,7 @@ export default [{
 
   description: {
     en: 'Your Precise Shot applies a poisonous Debuff on hit, damaging and slowing your enemies.',
-    ru: 'Ваши Precise Shots наносит ядовитый дебафф при ударе, повреждая и замедляя врагов.',
+    ru: 'Ваш Precise Shot наносит ядовитый дебафф при попадании, нанося урон и замедляя врагов.',
     fr: 'Votre Precise Shot applique un affaiblissment toxique à l\'impact, infligeant des dégats et ralentissant vos ennemis.'
   }
 },
@@ -273,7 +273,7 @@ export default [{
 
   description: {
     en: 'Place a totem on the ground, healing your entire party.',
-    ru: 'Положите тотем на землю, исцеляя всю вашу Party.',
+    ru: 'Расположите тотем на земле, который исцеляет всю вашу пати.',
     fr: 'Placez un totem sur le sol, soignant tout votre groupe.'
   }
 },
@@ -282,7 +282,7 @@ export default [{
 
   description: {
     en: 'Fire a Swift Shot which can be enhanced by casting a Precise Shot prior to it.',
-    ru: 'Сделайте Swift Shot, который можно усилить, сделав перед этим Precise Shot.',
+    ru: 'Выстрелите Swift Shot, который можно усилить, выстрелив перед этим Precise Shot.',
     fr: 'Tirez un Swift Shot, pouvant être amélioré s\'il est précédé d\'un Precise Shot.'
   }
 },
@@ -309,7 +309,7 @@ export default [{
 
   description: {
     en: 'Taunt surrounding enemies, forcing them to move towards you for a brief moment. Interrupts spell casts and resets aggro for all monsters.',
-    ru: 'Вы заставляете окружающих врагов двигаться к вам на короткое время. Прерывает произнесение заклинаний и сбрасывает агрессию для всех монстров.',
+    ru: 'Вы стягиваете врагов вокруг к вам на короткое время. Прерывает произнесение заклинаний и сбрасывает агрессию для всех монстров.',
     fr: 'Vous provoquez les ennemis aux alentours, les forçant à se déplacer vers vous pendant un bref instant. Interrompt les lancements de sort et réinitialise l\'aggro pour tous les monstres.'
   }
 },
@@ -318,7 +318,7 @@ export default [{
 
   description: {
     en: 'Summon your party members, allowing them to instantly teleport towards you.',
-    ru: 'Вызывайте членов своей party, позволяя им мгновенно телепортироваться к вам.',
+    ru: 'Призовите членов своей пати, позволяя им мгновенно телепортироваться к вам.',
     fr: 'Vous invoquez les membres de votre groupe, leur permettant de se téléporter instantanément à vous.'
   }
 },
@@ -327,7 +327,7 @@ export default [{
 
   description: {
     en: 'Turn into your spirit animal for additional movement speed, removing all movement impairing effects active at the point of the transformation. All spellcasting will cancel this effect.',
-    ru: 'Превратитесь в ваше духовное животное для дополнительного Movement Speed, удаляя все движения ухудшающие эффекты, активные в точке трансформации. Любое заклинание отменит этот эффект.',
+    ru: 'Превратитесь в ваше духовное животное для дополнительной Скорости Передвижения. Сбрасывает все замедляющие эффекты, которые были до трансформации. Любое заклинание отменит этот эффект.',
     fr: 'Vous vous transformez en votre animal spirituel pour obtenir de la vitesse de déplacement supplémentaire, supprimant tous les effets actifs altérant le mouvement au moment de la transformation. Tout lancé de sort annulera cet effet.'
   }
 },
@@ -336,7 +336,7 @@ export default [{
 
   description: {
     en: 'Turns your target into a zombie, interrupting all actions, slowing it down, and reducing received healing for the duration.',
-    ru: 'Превращает вашу цель в зомби, прерывая все действия, замедляя его и уменьшая полученное исцеление на время.',
+    ru: 'Превращает вашу цель в зомби, прерывая все действия, замедляя его и уменьшая получаемое им исцеление на время.',
     fr: 'Transforme votre cible en zombie, interrompant toutes ses actions, la ralentissant, et réduisant les soins qu\'elle reçoit pendant toute la durée de la transformation.'
   }
 },
@@ -345,7 +345,7 @@ export default [{
 
   description: {
     en: 'You dash into your current direction, instantly resetting the cooldown of Precise Shot. Your next Precise Shot is an instant cast.',
-    ru: 'Вы быстро двигаетесь в вашем текущем направлении, мгновенно сбросив перезагрузку Precise Shot. Ваш следующий Prec - мгновенный.',
+    ru: 'Вы делаете рывок в вашем текущем направлении, мгновенно сбросив перезарядку Precise Shot. Ваш следующий Precise Shot - мгновенный.',
     fr: 'Vous vous précipitez dans votre direction actuelle, réinitialisant instantanément le temps de recharge de Precise Shot. Votre prochain Precise Shot sera lancé instantanément.'
   }
 },
@@ -354,6 +354,7 @@ export default [{
 
   description: {
     en: 'Allows you to ride ground mounts. Mounts are bound to your account.',
+    ru: 'Позволяет вам оседлать Mounts (ездовых животных). Mounts привязаны к вашему аккаунту',
     fr: 'Vous permet de chevaucher des montures terrestres. Les mountures sont liées à votre compte.'
   }
 },
@@ -362,7 +363,7 @@ export default [{
 
   description: {
     en: 'Teleport to the nearest Conjurer.',
-    ru: 'Телепортируйтесь к Conjurer',
+    ru: 'Телепортируйтесь к Conjurer (на базу текищей локации)',
     fr: 'Vous vous téléportez au prestidigitateur le plus proche.'
   }
 },
